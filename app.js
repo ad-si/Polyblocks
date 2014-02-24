9ac450eb4d366deb99571fcce8b0ba3d1fe20f43
Dustin Gedlich <DasDing.GehtNicht@gmail.com>
2014-02-24 20:55:25 +0100

    cleaned up app.js and removed templating stuff

--- app.js
+++ app.js
@@ -3,34 +3,33 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	socketio = require('socket.io'),
+	polyblocks = require('./routes/polyblocks'),
+	app = express(),
+	server = http.createServer(app),
+	io = socketio.listen(server, {log: false})
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 8000)
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.methodOverride())
+app.use(app.router)
+app.use(express.static(path.join(__dirname, 'public')))
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler())
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// routes
+app.get('/', polyblocks.index)
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+server.listen(app.get('port'), function(){
+	console.log('Polyblocks server listening on port ' + app.get('port'))
+})
